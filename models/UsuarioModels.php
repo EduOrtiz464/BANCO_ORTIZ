a4756a3c0235e991fda6259657faa3458ba1b3b6
Edu Ortiz <eortizfray@gmail.com>
2026-09-01 21:10:20 -0500

    ESTOY AGREGANDO MAS CODIGO AL USUARIO MODELS

--- models/UsuarioModels.php
+++ models/UsuarioModels.php
@@ -15,6 +15,15 @@ class UsuarioModels {
     public function actualizarSaldo($id, $nuevoSaldo) {
         $sql = "UPDATE usuarios SET saldo = $nuevoSaldo WHERE id = $id";
         return $this->db->query($sql);
+      }  
+    public function listarUsuarios() {
+        $sql = "SELECT id, usuario, saldo FROM usuarios";
+        $resultado = $this->db->query($sql);
+        $usuarios = [];
+        while ($fila = $resultado->fetch_assoc()) {
+            $usuarios[] = $fila;
+        }
+        return $usuarios;
     }
 }
 ?>
